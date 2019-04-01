1241af7579fc77ea432deafb4a3bf1b67fa0c48a
unknown <unknown@example.com>
2019-04-02 01:28:38 +0800

    4月1日得更新。
    把js基础部分的文档重新翻了一遍。
    增加了备选框显示内容的功能，但暂时只能显示固定文本。增加了选择答案的功能，并可提供正确判定。但暂时还不能删除选项。

--- pages/index/index.js
+++ pages/index/index.js
@@ -23,9 +23,13 @@ Page({
         name: '疯狂的外星人'
       },
     ],
-    chooseid:(30),
+    chooseid:(10),//生成30个备选项方框
+    chooseworld:'这是象备选飞项的内小容哈哈,',//记录备选项的文本
+    nameme: '',//选择的答案
+    xuanxiang:0,//已选答案数
 
-  number:0,
+  number:0,//第几道题
+  nameLength:3,//记录电影名字长度
 
   chname:0,
   },
@@ -33,11 +37,33 @@ Page({
     var X=this.data.number;
     X+=1;
     this.setData({number:X})
-
+    this.setData({nameLength: this.data.Array[X].name.length})//计算当前电影名字总长。
+    this.setData({nameme:''})
+    this.setData({xuanxiang:0})
   },
   newtext:function(){
-
+    
+    var W=this.data.chooseworld[5];
+    this.setData({nameme:W})
+  
 
     
+  },
+  andaole:function(e){
+
+    if ( this.data.xuanxiang <this.data.nameLength){
+    var W=e.currentTarget.dataset.textt;
+    W=this.data.nameme+W;
+    this.setData({nameme:W});
+    console.log(this.data.nameme);
+    this.data.xuanxiang++;
+
+//--------------------------
+      var X = this.data.number;//结果判定
+      if (this.data.nameme == this.data.Array[X].name) {
+        console.log('答对了')
+      }
+    }
+
   }
 })
