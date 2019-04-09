edf48d2e046ef29236ee331041c0b78266a8b025
unknown <unknown@example.com>
2019-04-09 23:03:57 +0800

    19-04-09更新
    1.解决了随机生成备选答案并自动打乱顺序的问题。但是这个方案生成基本都是生僻字，下次还是改成高频字库好了。
    2.解决了30个备选框无法统一管理的问题。
    3.稍微优化了一下代码，增加了可读性。不然看着真的头疼。

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,6 @@ Page({
   /**
    * 页面的初始数据
    */
-
   data: {
     Array:[
       {
@@ -23,15 +22,13 @@ Page({
         name: '疯狂的外星人'
       },
     ],
+    lalala:[],
     chooseid:(10),//生成30个备选项方框
-    chooseworld:'这是象备选飞项的内小容哈哈,',//记录备选项的文本
+    chooseworld:'', //记录备选项的文本
     nameme: '',//选择的答案
     xuanxiang:0,//已选答案数
-
-  number:0,//第几道题
-  nameLength:3,//记录电影名字长度
-
-  chname:0,
+    number:0,//第几道题
+    nameLength:3,//记录电影名字长度
   },
   changeimage: function () {
     var X=this.data.number;
@@ -40,30 +37,51 @@ Page({
     this.setData({nameLength: this.data.Array[X].name.length})//计算当前电影名字总长。
     this.setData({nameme:''})
     this.setData({xuanxiang:0})
+    this.newtext()
   },
-  newtext:function(){
-    
-    var W=this.data.chooseworld[5];
-    this.setData({nameme:W})
-  
-
-    
+  newtext:function(){//随机生成字符补充缺位
+    /*测试代码。无用
+    //Unicode 中文范围 4E00 - 9FA5：CJK 统一表意符号(CJK Unified Ideographs)
+    //转换为10进制后中文范围从19968到40869
+  */ 
+  this.data.chooseworld=this.data.Array[this.data.number].name//将电影名称输入字符串
+    for(var i=this.data.chooseworld.length;i<30;i++){//补足缺位
+      var X=Math.round(Math.random()*20901+19968)
+      this.data.chooseworld +=String.fromCharCode(X)
+    }
+    var X = this.data.chooseworld
+    this.setData({chooseworld:X})//生成第一串字符串
+    console.log(this.data.chooseworld)//检查字符串
+    function upsetArr(arr) {//乱序函数
+      return arr.sort(function () { return Math.random() - 0.5 });
+    }
+    var txt=upsetArr(this.data.chooseworld.split("")).join("");//txt为打乱的字符串
+ var mimi=new Array()//暂存切分下来的字符串
+   mimi[0]= txt.substring(0,10)
+   mimi[1]=txt.substring(10,20)
+   mimi[2]=txt.substring(20,30)
+   this.setData({lalala:mimi})
+  console.log(this.data.lalala)
+  },
+  onLoad: function(){
+    this.newtext()
   },
   andaole:function(e){
-
     if ( this.data.xuanxiang <this.data.nameLength){
-    var W=e.currentTarget.dataset.textt;
-    W=this.data.nameme+W;
-    this.setData({nameme:W});
-    console.log(this.data.nameme);
-    this.data.xuanxiang++;
-
+      this.data.nameme += e.currentTarget.dataset.textt;
+      var txt=this.data.nameme
+      this.setData({nameme:txt})
+      console.log(this.data.nameme);
+      this.data.xuanxiang++;
 //--------------------------
       var X = this.data.number;//结果判定
       if (this.data.nameme == this.data.Array[X].name) {
         console.log('答对了')
+        this.changeimage()
       }
     }
+  },
+  xiugai:function(e){
 
   }
 })
