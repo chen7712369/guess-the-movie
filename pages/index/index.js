3d603aca6574201ae924e43b3b5d7aebb93e5d8c
unknown <unknown@example.com>
2019-05-19 22:44:53 +0800

    5-19更新
    解决了获取openid的问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,32 @@
+var movietxt=require('/movie.js');//载入目录文件
+//var moviejson=require('/movie.json')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    Array:[
+      Array:movietxt.indid,//将json发送的电影信息数据载入
+ //     TTTT:JSON.parse(url='/movie.json'),
+/*    Array:[
       {
-        src:'../image/小飞象.jpg', 
+        src:'../image/xfx.jpg', 
         name:'小飞象'
       },
       {
-        src: '../image/惊奇队长.jpg',
+        src: '../image/jqdz.jpg',
         name: '惊奇队长'
       },
       {
-        src: '../image/流浪地球.jpg',
+        src: '../image/lldq.jpg',
         name: '流浪地球'
       },
       {
-        src: '../image/疯狂的外星人.jpg',
+        src: '../image/fkdwxr.jpg',
         name: '疯狂的外星人'
       },
     ],
+    */
     lalala:[],//用来储存切割后的备选项
     chooseid:(10),//生成30个备选项方框
     binded: [],//记录选项的点击与否
@@ -32,8 +37,11 @@ Page({
     nameLength:3,//记录电影名字长度
     hunxiao:'的一是了我不人在他有这个上们来到时大地为子中你说生国年着就那和要她出也得里后自以会家可下而过天去能对小多然于心学么之都好看起发当没成只如事把还用第样道想作种开美总从无情己面最女但现前些所同日手又行意动方期它头经长儿回位分爱老因很给名法间斯知世什两次使身者被高已亲其进此话常与活正感',//储存常用字的字库
     changetxt:'',
+
+    movie:movietxt.indid,//把目录置入数据
+
   },
-  changeimage: function () {    
+  changeimage: function () {    //-----------------------------------------------------------------------------
     var X=this.data.number;
     X+=1;
     this.setData({number:X})
@@ -41,17 +49,16 @@ Page({
     this.setData({ xuanxiang: 0 })
     this.setData({ changetxt: '点击切换下一张图片'})
     this.newtext()
-    this.Nnameme=function(){
-    }
-
+    //将该变量重置为一串和答案等长的空格
     var txt = ''
     for (var i = 0; i < this.data.nameLength; i++) {
       txt += ' '
     }
-    this.setData({ nameme: txt })//将该变量重置为一串和答案等长的空格
+    this.setData({ nameme: txt })
+    //
     this.newbindedtap()
   },
-  newtext:function(){//随机生成字符补充缺位
+  newtext: function () {//随机生成字符补充缺位-----------------------------------------------------------------------------
   this.data.chooseworld=this.data.Array[this.data.number].name//将电影名称输入字符串
     for(var i=this.data.chooseworld.length;i<30;i++){//补足缺位
       var X=Math.round(Math.random()*139)
@@ -72,15 +79,19 @@ Page({
    this.setData({lalala:mimi})
   //console.log(this.data.lalala)//检测词条生成状况
   },
-  onLoad: function(){//加载完成后先扫一遍
+  onLoad: function () {//加载完成后先扫一遍-----------------------------------------------------------------------------
     this.changeimage()
+ 
+  //  var mivie=this.data.moviee.indid
+  //  console.log(this.data.movie[0].src)
+
   },
-  newbindedtap: function () {//将备选框点击状态置0
+  newbindedtap: function () {//将备选框点击状态置0-----------------------------------------------------------------------------
     for (var m = 0; m < 30; m++) {
       this.data.binded[m] = 0
     }
   },
-  andaole:function(e){//处理点击备选框的事件
+  andaole: function (e) {//处理点击备选框的事件-----------------------------------------------------------------------------
     if(this.data.binded[Number(e.currentTarget.dataset.bindedtap)]==0){//判断选项未被点击过
       if ( this.data.xuanxiang <this.data.nameLength){//判断选项是否已满
         for(var i=0;i<this.data.nameLength;i++){//寻找空位
@@ -104,7 +115,7 @@ Page({
       }
     }
   },
-  xiugai:function(e){//处理点击答案框的修改事件
+  xiugai: function (e) {//处理点击答案框的修改事件-----------------------------------------------------------------------------
     if (this.data.nameme[e.currentTarget.dataset.number]!=' '){
       for (var i = 0; i < 30; i++) {
         if (this.data.chooseworld[i] == this.data.nameme[e.currentTarget.dataset.number] && this.data.binded[i] == 1) {//寻找当前删除的地府对应的第一个点击过的备选字
@@ -112,11 +123,11 @@ Page({
           break;
         }
       }
-      console.log('跳出')
+   //   console.log('跳出')
       var W = e.currentTarget.dataset.number//取出被点击的放个所在位
       var txt = this.data.nameme.substring(0, W) +' '  + this.data.nameme.substring(W + 1, this.data.nameLength)
       this.setData({ nameme: txt })
-      console.log(this.data.nameme)
+    //  console.log(this.data.nameme)
       this.data.xuanxiang--;
     }
   }
